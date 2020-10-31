68755798f90afbf7a3a8b8c5cd3f5af9972c8b79
articbear1999 <jacobchenlin@gmail.com>
2020-10-30 17:24:21 -0700

    94.07

--- testcases/hw2/Basic.java
+++ testcases/hw2/Basic.java
@@ -1,13 +1,17 @@
 class Main {
-	public static void main(String[] a){
+	public static void main(String[] y){
+
 		System.out.println(new A().run());
 	}
 }
 
-class A {
+class A extends Main{
 	public int run() {
 		int x;
-		y = 1; // TE: Name error!  Variable 'y' doesn't exist.
+		int a;
+		a = 1; // TE: Name error!  Variable 'y' doesn't exist.
 		return x;
 	}
 }
+
+
